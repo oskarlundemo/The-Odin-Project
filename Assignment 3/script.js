bfaed26a5166f9bfd545c24018dd207e6c54c49e
olundemo <bjornoskarstromberg@gmail.com>
2024-01-01 17:38:24 +0100

    Checkpoint

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -2,13 +2,47 @@
 
 $(document).ready(function () {
 
+    let mouseMove = $('#boxArea');
+    mouseMove.on('mouseover', ChangeDivColor);
 
-    let box = $('#boxArea');
+
 
     for (i = 0; i < 16; i ++) {
         $('#boxArea').append($('<div class="mouseArea"></div>'))
+
     }
 
+    function ChangeDivColor () {
 
+        let x = 0;
+        let y = 0;
+
+
+        $('.mouseArea').on("mousemove", function (event) {
+            x = event.pageX;
+            y = event.pageY;
+
+            console.log(event.pageX + ", " + event.pageY);
+
+            let test = TargetedElemnt(x,y);
+
+            test.style.backgroundColor = 'black';
+        });
+
+
+    }
 
 });
+
+
+
+function TargetedElemnt (x,y) {
+
+
+    let target = document.elementFromPoint(x, y);
+
+    if (target === $('mouseArea'))
+        alert('hej')
+
+    return target;
+}
