1502ede53bb5dfe4bf760109705bea5ce138e474
olundemo <bjornoskarstromberg@gmail.com>
2024-01-14 15:58:06 +0100

    Cleaning up the css code!

--- Assignment 4/script.js
+++ Assignment 4/script.js
@@ -1,2 +1,41 @@
 
 
+
+
+$(document).ready(function () {
+
+
+    let totalSum = 0;
+    let currentNumbers = ''
+
+    let secondString = '';
+    let currentOperator = '';
+
+
+    $('.button').on('click', function (e) {
+
+        if (currentNumbers !== null) {
+            secondString += this.id;
+        }
+
+        currentNumbers += this.id.toString();
+        $('#displayNumbers').text(currentNumbers);
+
+    });
+
+
+    $('.operation').on('click', function (e) {
+
+
+        $('#displayNumbers').text('');
+        currentOperator = this.id;
+
+        console.log(currentOperator)
+    })
+
+
+
+
+
+
+});
