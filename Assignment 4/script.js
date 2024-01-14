e604037790ba3d186fe0916a41b78210262992a1
olundemo <bjornoskarstromberg@gmail.com>
2024-01-14 17:52:24 +0100

    Added the oprerators.

--- Assignment 4/script.js
+++ Assignment 4/script.js
@@ -4,38 +4,103 @@
 
 $(document).ready(function () {
 
-
-    let totalSum = 0;
-    let currentNumbers = ''
-
-    let secondString = '';
     let currentOperator = '';
 
+    let currentNumbers = ''
+    let secondString = '';
+
+    let totalSum = 0;
+
+
+    $('#reset').on('click', function (e) {
+
+        $('#displayNumbers').text('0');
+
+        currentNumbers = '';
+        secondString = '';
+    });
 
     $('.button').on('click', function (e) {
 
-        if (currentNumbers !== null) {
+        if (ContainsOperator(currentNumbers)) {
             secondString += this.id;
+            $('#displayNumbers').text(secondString);
+        } else {
+            currentNumbers += this.id.toString();
+            $('#displayNumbers').text(currentNumbers);
         }
-
-        currentNumbers += this.id.toString();
-        $('#displayNumbers').text(currentNumbers);
 
     });
 
 
+    $('#sum').on('click', function (e) {
+
+        $('.operation').css('background-color', "");
+        totalSum = StringToNumber(currentNumbers, secondString, currentOperator)
+
+        $('#displayNumbers').text(totalSum.toString());
+
+        console.log(totalSum);
+
+        secondString = '';
+        currentNumbers = totalSum;
+    });
+
+
+    function StringToNumber (firstString, secondString, operator) {
+
+        let firstSeq = parseFloat(firstString);  // Use parseFloat for floating-point numbers
+        let secondSeq = parseFloat(secondString);
+
+        let result = 0;
+
+        switch (operator) {
+            case '+':
+                result = firstSeq + secondSeq;
+                break;
+
+                case '-':
+                    result = firstSeq - secondSeq;
+                    break;
+
+
+                case '*':
+                 result = firstSeq * secondSeq;
+                 break;
+
+
+                case '/':
+                    result = firstSeq / secondSeq;
+                    break;
+
+
+                    case '':
+                        result = 0;
+                        break;
+            default:
+                console.log('Invalid operator');
+                break;
+        }
+
+        return result;
+    }
+
+
     $('.operation').on('click', function (e) {
 
+        $('.operation').css('background-color', "");
+
+        $(this).css('background-color', 'darkorange');
 
         $('#displayNumbers').text('');
         currentOperator = this.id;
 
-        console.log(currentOperator)
+        currentNumbers = currentNumbers + currentOperator;
+
     })
 
 
-
-
-
-
+    function ContainsOperator (stringOfNumbers) {
+        return /\+|-|\*|\//.test(stringOfNumbers);
+    }
 });
