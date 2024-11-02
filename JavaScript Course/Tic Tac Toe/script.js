24e7e99ed021ea4882d8b91a0b4eb8a1818fd67b
olundemo <bjornoskarstromberg@gmail.com>
2024-11-02 20:52:17 +0100

    Made a spinning circle

--- JavaScript Course/Tic Tac Toe/script.js
+++ JavaScript Course/Tic Tac Toe/script.js
@@ -4,5 +4,25 @@
 document.addEventListener('DOMContentLoaded', function (e) {
 
     const square = document.getElementsByClassName('cell');
+    const startButton = document.getElementById('startBtn')
+
+    const overlay = document.getElementById('overlay');
+    const greetingsMenu = document.getElementById('userGreeting');
+
+    startButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        greetingsMenu.classList.remove('active');
+    })
+
+
+    let spinBtn = document.getElementById('spinBtn');
+    let wheel = document.getElementById('wheel');
+
+    spinBtn.addEventListener('click', function (e) {
+        console.log("Snart spyr jag")
+        let number = Math.ceil(Math.random() * 10000)
+        wheel.style.transform = "rotate(" + number + "deg)";
+        console.log(number);
+    })
 
 })
