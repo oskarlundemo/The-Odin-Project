c62955a5154391fd75ab9e73fb550a1e7cebd446
olundemo <bjornoskarstromberg@gmail.com>
2024-11-08 11:28:41 +0100

    Mainly updates on the css. Also starting to workout the modules inside the js files.

--- JavaScript Course/Todo/js/app.js
+++ JavaScript Course/Todo/js/app.js
@@ -1,6 +1,13 @@
 
 
 
+import {createTodoElement} from './dom.js';
 
 
+document.addEventListener("DOMContentLoaded", () => {
 
+
+    const addTodoButton = document.getElementById("addTodoBtn").addEventListener("click", () => {
+        createTodoElement();
+    })
+})
