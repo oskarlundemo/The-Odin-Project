4e9ba31e60eeb0e5d93dffd0b24415bb6f81d95a
olundemo <bjornoskarstromberg@gmail.com>
2024-12-09 20:58:48 +0100

    Bigger UI changes

--- JavaScript Course/Todo/js/app.js
+++ JavaScript Course/Todo/js/app.js
@@ -10,15 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const projects = new Project();
 
-
     const addTodoButton = document.getElementById("addTodoBtn").addEventListener("click", () => {
         const checkForProject = document.getElementById("projects")
+        const currentProject = document.getElementById('projectTitle');
 
-        if (checkForProject.querySelector("li")) {
+        if (checkForProject.querySelector("li") && currentProject.textContent !== '') {
             createTodoElement(projects);
         }
         else {
-            alert('Please create a project first');
+            alert('Please create and select an project first');
         }
     })
 
