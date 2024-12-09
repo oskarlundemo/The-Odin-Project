e651bb36e2b74a8300c565d9c468f64189f28c45
olundemo <bjornoskarstromberg@gmail.com>
2024-12-09 20:58:48 +0100

    Bigger UI changes

--- JavaScript Course/Todo/js/dom.js
+++ JavaScript Course/Todo/js/dom.js
@@ -8,7 +8,6 @@ export function createProjectElement (projects) {
     const projectDescription = document.createElement('input');
     projectDescription.classList.add('hidden');
 
-
     const newProject = document.createElement('li');
     const listOfProjects = document.getElementById("projects");
     const currentProject = document.getElementById('projectTitle')
@@ -16,13 +15,15 @@ export function createProjectElement (projects) {
 
     if (listOfProjects.lastElementChild === null ||
         listOfProjects.lastElementChild.tagName.toLowerCase() !== 'input') {
-        projectDescription.classList.remove('hidden');
+
+        setTimeout(() => {
+            projectDescription.classList.remove('hidden');
+        }, 10);
+
         listOfProjects.append(projectDescription);
     }
 
-
     listOfProjects.addEventListener('click', function (e) {
-        const mainGrid = document.getElementById('mainGrid');
 
         if(e.target && e.target.nodeName === 'LI') {
 
@@ -41,7 +42,7 @@ export function createProjectElement (projects) {
             }, 500);
         }
 
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' && projectDescription.value.trim().length > 0) {
             projectDescription.classList.add('hidden');
 
             setTimeout(() => {
@@ -64,7 +65,6 @@ export function createProjectElement (projects) {
 
 
 
-
 function clearMainGrid () {
     const mainGrid = document.getElementById('mainGrid');
 
@@ -75,22 +75,16 @@ function clearMainGrid () {
 
 
 
-
 function renderTodos (projects, current){
 
     const currentLists = [];
     const mainGrid = document.getElementById('mainGrid');
 
     projects.projectData.forEach((item, index) => {
-
         if (item.id === current) {
             currentLists.push(...item.todos); // Spread the todos array into currentLists
         }
     });
-
-    console.log(currentLists);
-    console.log(projects);
-
 
     currentLists.forEach(todo => {
 
@@ -98,36 +92,84 @@ function renderTodos (projects, current){
         card.classList.add('todoCard');
 
 
+        const headerIcons = document.createElement('div');
+        const editIcon = document.createElement('img');
+        editIcon.src = './icons/edit.svg';
+        const deleteIcon = document.createElement('img');
+        deleteIcon.src = './icons/delete.svg';
+
+        headerIcons.appendChild(editIcon);
+        headerIcons.appendChild(deleteIcon);
+
+
         const date = document.createElement('input');
         date.type = 'date';
         date.value = todo.dueDate;
+        date.disabled = true;
 
-        const title = document.createElement('h2');
+        const title = document.createElement('input');
         title.textContent = todo.title;
+        title.disabled = true;
 
-        const description = document.createElement('p');
+        const description = document.createElement('textarea');
         description.textContent = todo.description;
+        description.disabled = true;
+
+        const ratingDiv = document.createElement('div');
+        const urgencyRating = document.createElement('select');
+        urgencyRating.name = 'urgencyRating';
+        const urgencyLabel = document.createElement('label');
+        urgencyLabel.innerText = 'Urgency';
+        urgencyLabel.htmlFor = 'urgencyRating';
+
+        ratingDiv.append(urgencyLabel);
+        ratingDiv.append(urgencyRating);
+        urgencyRating.disabled = true;
+
+
+        ['Low', 'Medium', 'High'].forEach(optionText => {
+            const option = document.createElement('option');
+            option.value = optionText.toLowerCase();
+            option.textContent = optionText;
+            urgencyRating.appendChild(option);
+        });
+
+
+        urgencyRating.value = todo.priority;
 
         const priority = document.createElement('p');
         priority.textContent = `Priority: ${todo.priority}`;
 
+
+        const footerContainer = document.createElement('div');
         const completeCheckbox = document.createElement('input');
         completeCheckbox.type = 'checkbox';
         completeCheckbox.checked = todo.complete;
+        completeCheckbox.id = 'completeCheckbox';
+
+        const labelComplete = document.createElement('label');
+        labelComplete.innerText = 'Completed';
+        labelComplete.htmlFor = 'completeCheckbox';
+
+        footerContainer.appendChild(labelComplete);
+        footerContainer.appendChild(completeCheckbox);
 
 
+        card.appendChild(headerIcons);
         card.appendChild(date);
         card.appendChild(title);
         card.appendChild(description);
-        card.appendChild(priority);
-        card.appendChild(completeCheckbox);
-
-        console.log(todo);
+        card.appendChild(ratingDiv);
+        card.appendChild(footerContainer);
 
         mainGrid.appendChild(card);
     })
 
 }
+
+
+
+
 
 
 export function createTodoElement (projects) {
@@ -197,5 +239,7 @@ export function createTodoElement (projects) {
         newCard.classList.add('todoCard')
     }, 10);
 
+
+    if (mainGrid)
     mainGrid.append(newCard);
 }
