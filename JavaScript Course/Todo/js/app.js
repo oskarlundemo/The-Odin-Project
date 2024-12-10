681be7d1556eaae3b297ee6ff1b787ed996a3acd
olundemo <bjornoskarstromberg@gmail.com>
2024-12-10 13:34:24 +0100

    Last stretch with deleteing and editing

--- JavaScript Course/Todo/js/app.js
+++ JavaScript Course/Todo/js/app.js
@@ -10,6 +10,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const projects = new Project();
 
+    projects.addProject("Skola");
+    const skolis = new Todo("Plugga", "Läsa minst två kapitel i ekonomistyrning",
+        "2022-01-15", 3);
+
+    projects.projectData[0].addTodo(skolis);
+
+    console.log(skolis);
+    console.log(projects.projectData[0].todos[0].id)
+
+    projects.projectData[0].todos[0].title = "Dansa";
+
+    console.log(skolis);
+
+
+
+
     const addTodoButton = document.getElementById("addTodoBtn").addEventListener("click", () => {
         const checkForProject = document.getElementById("projects")
         const currentProject = document.getElementById('projectTitle');
