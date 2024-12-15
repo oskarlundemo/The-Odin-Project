85d42080c09747dbb72807a316b78918b6ca3fa2
olundemo <bjornoskarstromberg@gmail.com>
2024-12-15 10:03:31 +0100

    Working with an API
    Just updating the CSS

--- JavaScript Course/Weather App/js/dom.js
+++ JavaScript Course/Weather App/js/dom.js
@@ -2,10 +2,25 @@
 
 
 
-
-
-export function swipeRight () {
+export function start () {
 
     const header = document.querySelector('header')
     header.classList.add('swipeRight')
+    const sideBar = document.querySelector('aside')
+    sideBar.classList.remove('hidden')
+
+    header.classList.add('hidden')
 }
+
+
+document.addEventListener('DOMContentLoaded', function (ev) {
+
+
+    const sideBar = document.querySelector('aside');
+
+    sideBar.addEventListener('drag', function (ev) {
+        console.log(ev.clientX)
+    })
+
+})
+
