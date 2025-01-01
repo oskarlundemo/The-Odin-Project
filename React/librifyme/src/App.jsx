279d71214be9835066ec45c9cf4f00b0b6c58257
olundemo <bjornoskarstromberg@gmail.com>
2025-01-01 15:50:39 +0100

    Working on LibrifyMe 2.0

---
+++ React/librifyme/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Home from "../pages/Home.jsx";
+import {Routes, Route} from 'react-router-dom'
+import Favorites from "../pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
+
+function App() {
+
+  return (
+
+      <div>
+          <NavBar/>
+          <main className="main-content">
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/favorites" element={<Favorites/>}/>
+              </Routes>
+          </main>
+      </div>
+  )
+}
+
+export default App
