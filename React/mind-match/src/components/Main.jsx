344eb77d72e92391468473d3d42fb565df1b99b7
olundemo <bjornoskarstromberg@gmail.com>
2025-01-11 20:23:13 +0100

    Add global state management and improve game logic
    Introduce `AppProvider` for global state to manage points, high scores, and clicked Pokémons. Update components to utilize context for shared state and implement shuffle functionality after each click. Enhance gameplay with high score tracking and reset on repeated clicks.

--- React/mind-match/src/components/Main.jsx
+++ React/mind-match/src/components/Main.jsx
@@ -1,7 +1,7 @@
-import {useEffect, useState} from "react";
 import {getPokemons} from "../services/api.js";
 import {PokeCard} from "./PokeCard.jsx";
 import '../styles/Main.css'
+import {useEffect, useState} from "react";
 
 
 export const Main = () => {
@@ -16,11 +16,21 @@ export const Main = () => {
         loadPokemonsData();
     }, []);
 
+    const shufflePokedeck = () => {
+        const shuffled = [...pokemons].sort(() => Math.random() -0.5)
+        setPokemons(shuffled);
+    }
+
+
     return (
         <main className="main-content">
                 {pokemons.map(pokemon => (
-                    <PokeCard pokemon={pokemon} key={pokemon.id}/>
-                ))}
+                    <PokeCard
+                        pokemon={pokemon}
+                        key={pokemon.id}
+                        shufflePokemons={shufflePokedeck}
+                    />))
+                }
         </main>
     )
 }
--- React/mind-match/src/components/PokeCard.jsx
+++ React/mind-match/src/components/PokeCard.jsx
@@ -1,14 +1,41 @@
 
 import '../styles/PokeCard.css'
+import {AppContext} from "../../context/AppProvider.jsx";
+import {useContext} from "react";
 
 
 
 
-export const PokeCard = ({pokemon}) => {
+export const PokeCard = ({pokemon, shufflePokemons}) => {
+
+    const {setPoints, clickedPokemon, setClickedPokemon, highScore, setHighScore} = useContext(AppContext);
+
+    const handleClick = () => {
+        if (!clickedPokemon[pokemon.id]) {
+            setPoints((prev) => {
+                const newPoints = prev + 1;
+                if (newPoints > highScore) {
+                    setHighScore(newPoints);
+                }
+
+                return newPoints;
+            })
+            setClickedPokemon((prev) => ({
+                ...prev,
+                [pokemon.id]: true,
+            }));
+        } else {
+            setPoints(0);
+            setClickedPokemon({});
+        }
+    }
 
     return (
-        <div className="pokemon-card">
-            <img src={pokemon.image} alt={pokemon.name} />
+        <div onClick={() => {
+            handleClick(),
+            shufflePokemons();
+        }} className="pokemon-card">
+            <img src={pokemon.image} alt={pokemon.name}/>
             <h2>{pokemon.name}</h2>
         </div>
     )
